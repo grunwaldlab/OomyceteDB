30237b2cfee8001d41de7b8e0c4977512480f963
zachary-foster <zacharyfoster1989@gmail.com>
2019-05-31 12:33:39 -0700

    another try

--- shiny_apps/blast/ui.r
+++ shiny_apps/blast/ui.r
@@ -54,7 +54,7 @@ ui <- fluidPage(tagList(
                 # this snippet generates a progress indicator for long BLASTs
                 div(class = "busy",  
                     p("Calculation in progress.."), 
-                    img(src = "www/progress.gif", height = 100, width = 100, align = "center")
+                    img(src = "http://oomycetedb.cgrb.oregonstate.edu/images/progress.gif", height = 100, width = 100, align = "center")
                 ),
                 
                 # Basic results output
